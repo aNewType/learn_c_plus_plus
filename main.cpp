6f981254a4398b41253f01c732de9ebcaeba4f80
aNewType <owensowens250@gmail.com>
2025-04-03 17:36:48 +0800

    take one function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+void Log(const char* message)
+{
+	std::cout << message << std::endl;
+}
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	Log("Hello World!");
 	std::cin.get();
 	return 0;
 }
