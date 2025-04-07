63f11938d268685a6915dde0561ad6b0807b4338
aNewType <owensowens250@gmail.com>
2025-04-07 20:09:41 +0800

    use functions

--- main.cpp
+++ main.cpp
@@ -3,13 +3,36 @@
 
 static int Multiply(int a, int b)
 {
-	Log("Multiply");
 	return a * b;
+}
+
+void MultiplyAndLog(int a, int b)
+{
+	int result = Multiply(a, b);
+	std::cout << result << std::endl;
 }
 
 int main()
 {
-	std::cout << Multiply(5, 8) << std::endl;
+	unsigned int variable = 8;
+	std::cout << variable << std::endl;
+	variable = 20;
+	std::cout << variable << std::endl;
+
+	short a = 'A';
+	std::cout << a << std::endl;
+
+	float variable1 = 5.5f;
+	double var = 5.2;
+	std::cout << variable1 << std::endl;
+
+	bool variable2 = false;
+	std::cout << variable2 << std::endl;
+	std::cout << sizeof(bool) << std::endl;
+
+	MultiplyAndLog(3, 2);
+	MultiplyAndLog(8, 5);
+	MultiplyAndLog(90, 45);
 	std::cin.get();
 	return 0;
 }
