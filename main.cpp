8500a6931180e7f68bbac84275c205249540e385
aNewType <153738504+aNewType@users.noreply.github.com>
2025-04-20 00:58:39 +0800

    use "new" to create a pointer and use "delete" to delete a pointer

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 
 int main()
 {
-	int var = 8;
-	int* ptr = &var;
-	*ptr = 10;
-	LOG(var);
+	char* buffer = new char[8];
+	memset(buffer, 0, 8);
+
+	char** ptr = &buffer;
+
+	delete[] buffer;
 	std::cin.get();
 }
