2c688d4ebb76027ab108bbeb4fc25d59236f02d0
aNewType <153738504+aNewType@users.noreply.github.com>
2025-04-23 16:10:25 +0800

    compare different ways to change a variable

--- main.cpp
+++ main.cpp
@@ -2,13 +2,41 @@
 
 #define LOG(x) std::cout << x << std::endl;
 
+void Increment(int value)
+{
+	value++;
+}
+
+void Increment_ptr(int* value)
+{
+	(*value)++;
+}
+
+void Increment_ref(int& value)
+{
+	value++;
+}
+
 int main()
 {
-	char* buffer = new char[8];
-	memset(buffer, 0, 8);
+	int a = 5;
+	// not to do any else to addition a variable as comparison
+	Increment(a);
+	LOG(a);
 
-	char** ptr = &buffer;
+	// use a pointer to modify variable(a)
+	Increment_ptr(&a);
+	LOG(a);
 
-	delete[] buffer;
+	// use a reference to modify variable(a) int a function
+	a = 5;
+	Increment_ref(a);
+	LOG(a);
+	
+	// use a reference to modify the variable(a)
+	int& ref = a;
+	a = 2;
+	LOG(a);
+
 	std::cin.get();
 }
