c894a8214419814d71fd3ec117cf2f513d18583e
aNewType <153738504+aNewType@users.noreply.github.com>
2025-04-23 18:14:24 +0800

    use static out of the class or struct

--- main.cpp
+++ main.cpp
@@ -1,44 +1,9 @@
 #include <iostream>
 
-class Log
-{
-public:
-	const int LogLevelError = 0;
-	const int LogLevelWarning = 1;
-	const int LogLevelInfo = 2;
-private:
-	int m_Loglevel = LogLevelInfo;
-public:
-	void SetLevel(int level)
-	{
-		m_Loglevel = level;
-	}
-
-	void Error(const char* message)
-	{
-		if(m_Loglevel >= LogLevelError)
-			std::cout << "[ERROR]: " << message << std::endl;
-	}
- 
-	void Warn(const char* message)
-	{
-		if (m_Loglevel >= LogLevelWarning)
-			std::cout << "[WARNING]: " << message << std::endl;
-	}
-
-	void Info(const char* message)
-	{
-		if (m_Loglevel >= LogLevelInfo)
-			std::cout << "[INFO]: " << message << std::endl;
-	}
-};
+extern int s_Variable;
 
 int main()
 {
-	Log log;
-	log.SetLevel(log.LogLevelError);
-	log.Error("Hello!");
-	log.Warn("Hello!");
-	log.Info("Hello!");
+	std::cout << s_Variable << std::endl;
 	std::cin.get();
 }
