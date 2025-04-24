a04d08fbfa2c7f74f42ddf9c50b53b96a3f94064
aNewType <153738504+aNewType@users.noreply.github.com>
2025-04-24 15:49:50 +0800

    static in class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
-extern int s_Variable;
+struct Entity
+{
+	static int x, y;
+
+	static void Print()
+	{
+		std::cout << x << ", " << y << std::endl;
+	}
+};
 
 int main()
 {
-	std::cout << s_Variable << std::endl;
+	Entity e;
+	e.x = 2;
+	e.y = 3;
+
+	Entity e1;
+	e1.x = 5;
+	e1.y = 8;
+
+	Entity::Print();
+	Entity::Print();
 	std::cin.get();
 }
