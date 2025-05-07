6bd923f6a790e17cbe14413465a27298f71c35d7
aNewType <153738504+aNewType@users.noreply.github.com>
2025-05-08 03:08:27 +0800

    the visibility of variable and function in class

--- main.cpp
+++ main.cpp
@@ -1,64 +1,35 @@
 #include <iostream>
 #include <string>
 
-class Printable
+class Entity
 {
 public:
-	virtual std::string GetClassName() = 0;
-};
+	int X, Y;
 
-class Entity : public Printable
-{
+	void Print() {}
+
 public:
-	virtual std::string GetName() { return "Entity"; }
-
-	std::string GetClassName() override { return "Entity"; }
+	Entity()
+	{
+		X = 0;
+		Print();
+	}
 };
 
 class Player : public Entity
 {
-private:
-	std::string m_Name;
 public:
-	Player(const std::string& name)
-		: m_Name(name) {}
-
-	std::string GetName() override { return m_Name; }
-	
-	 std::string GetClassName() override { return "Player"; }
+	Player()
+	{
+		X = 2;
+		Print();
+	}
 };
-
-void PrintName(Entity* entity)
-{
-	std::cout << entity->GetName() << std::endl;
-}
-
-class A : public Printable
-{
-public:
-	std::string GetClassName() override { return "A"; }
-};
-
-void Print(Printable* obj)
-{
-	std::cout << obj->GetClassName() << std::endl;
-}
 
 int main()
 {
-	Entity* e = new Entity();
-	//std::cout << e->GetName() << std::endl;
-	//PrintName(e);
-	
-	Player* p = new Player("cherno");
-	//std::cout << p->GetName() << std::endl;
-	//PrintName(p);
-
-	//Entity* entity = p;
-	//std::cout << entity->GetName() << std::endl;
-
-	Print(e);
-	Print(p); 
-	Print(new A());
+	Entity e;
+	e.Print();
+	e.X = 2;
 	std::cin.get();
 } 
