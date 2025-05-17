e22b1604899f05f7e1eafc5460cb493925e1bca9
aNewType <153738504+aNewType@users.noreply.github.com>
2025-05-17 19:03:23 +0800

    the string litre

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <string>
-
-void PrintString(const std::string& name)
-{
-	std::cout << name << std::endl;
-}
+#include <stdlib.h>
 
 int main()
 {
-	std::string name = std::string("cherno") + " hello!";
-	PrintString(name);
-	bool contains = name.find("no") != std::string::npos;
+	using namespace std::string_literals;
+
+	std::u32string name0 = U"cherno"s + U"hello";
+
+	const char* example = R"(Line1
+Line2
+Line3
+Line4)";
+
+	const char* ex = "Line1\n"
+		"Line2\n"
+		"Line3\n";
+
+	const char* name = "cherno";	// 1 bytes, also can say: const char* name = u8"cherno";
+	std::cout << strlen(name) << std::endl;
 	std::cout << name << std::endl;
 
- 	std::cin.get();
+	const wchar_t* name2 = L"cherno";	// 2 bytes or 4 bytes
+	const char16_t* name3 = u"cherno";	// 2 bytes
+	const char32_t* name4 = U"cherno";	// 4 bytes
+ 	
+	std::cin.get();
 } 
