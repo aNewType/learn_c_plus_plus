23cdbf964cf9add99507a0386ce4481dcd8fb108
aNewType <153738504+aNewType@users.noreply.github.com>
2025-05-17 17:04:40 +0800

    string in c++

--- main.cpp
+++ main.cpp
@@ -1,44 +1,17 @@
 #include <iostream>
-#include <array>
+#include <string>
 
-class Entity
+void PrintString(const std::string& name)
 {
-public:
-	static const int exampleSize = 5;
-	int example[exampleSize];
-	//int* example = new int[5];
-
-	std::array<int, 5> another;
-
-	Entity()
-	{
-		for (int i = 0; i < exampleSize; i++)
-			example[i] = 2;
-
-		for (int i = 0; i < another.size(); i++)
-			another[i] = 2;
-	}
-};
+	std::cout << name << std::endl;
+}
 
 int main()
 {
-	// int example[5];
-	// int* ptr = example;
-
-	//for (int i = 0; i < 5; i++)
-	//	example[i] = 2;
-
-	//example[2] = 5;
-	//*(int*)((char*)ptr + 8) = 6;	// *(ptr + 2) = 6;
-
-	Entity e;
-
-	/*int* another = new int[5];
-	
-	for (int i = 0; i < 5; i++)
-		another[i] = 2;
-	
-	delete[] another;*/
+	std::string name = std::string("cherno") + " hello!";
+	PrintString(name);
+	bool contains = name.find("no") != std::string::npos;
+	std::cout << name << std::endl;
 
  	std::cin.get();
 } 
