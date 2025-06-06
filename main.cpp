aabcc41dfc1eaca8b1282060a10a646256a7d94a
aNewType <153738504+aNewType@users.noreply.github.com>
2025-06-07 03:07:07 +0800

    use string

--- main.cpp
+++ main.cpp
@@ -1,49 +1,29 @@
 #include <iostream>
 #include <string>
 
-class Example
-{
-private:
-
-public:
-	Example()
-	{
-		std::cout << "Create Entity!" << std::endl;
-	}
-
-	Example(int x)
-	{
-		std::cout << "Create Entity with " << x << "!" << std::endl;
-	}
-};
-
-class Entity
-{
-private:
-	int m_Score;
-	std::string m_Name;
-	Example m_Example;
-
-public:
-	Entity()
-		: m_Score(0), m_Name("Unknow"), m_Example(8)
-	{
-	}
-
-	Entity(const std::string& name)
-		: m_Score(0), m_Name(name)
-	{
-	}
-
-	const std::string& GetName() const 
-	{ 
-		return m_Name;  
-	}
-};
+static int s_Level = 8;
+static int s_Speed = 2;
 
 int main()
 {
-	Entity e0;
+	if (s_Level > 5)
+		s_Speed = 10;
+	else
+		s_Speed = 5;
+	std::cout << s_Speed << std::endl;
+
+	s_Speed = s_Level > 5 ? s_Level > 10 ? 15 : 10 : 5;
+	std::cout << s_Speed << std::endl;
+
+	std::string rank = s_Level > 10 ? "Master" : "Beginner";
+	std::cout << rank << std::endl;
+
+	std::string otherRank;
+	if (s_Level > 10)
+		otherRank = "Master";
+	else
+		otherRank = "Beginner";
+	std::cout << otherRank << std::endl;
 
 	std::cin.get();
 } 
